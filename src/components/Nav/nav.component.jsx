f8301b8bb6fd0ade1510eca9c122f4d1d5b1c7a6
Shaikh Yakub Hossain <shaikhyakubhossain@gmail.com>
2023-02-10 18:08:32 +0530

    Add Menu Button

--- src/components/Nav/nav.component.jsx
+++ src/components/Nav/nav.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from '../Nav/nav.module.css';
+import { IconButton } from '@mui/material/';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Nav = () => {
   return (
     <div className={`${styles.mainContainer} ${styles.allContainers}`}>
         <div className={styles.leftContainer}>
-          
+          <IconButton><MenuIcon /></IconButton>
+          <div></div>
         </div>
         <div className={styles.middleContainer}>
           
