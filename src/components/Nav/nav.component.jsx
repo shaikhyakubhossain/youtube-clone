9303db237549d657a6fe3602f350433a23fae9e3
Shaikh Yakub Hossain <shaikhyakubhossain@gmail.com>
2023-02-11 20:14:41 +0530

    Add Youtube-Clone Logo

--- src/components/Nav/nav.component.jsx
+++ src/components/Nav/nav.component.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import styles from '../Nav/nav.module.css';
 import { IconButton } from '@mui/material/';
 import MenuIcon from '@mui/icons-material/Menu';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 const Nav = () => {
   return (
     <div className={`${styles.mainContainer} ${styles.allContainers}`}>
-        <div className={styles.leftContainer}>
+        <div className={`${styles.leftContainer} ${styles.allContainers}`}>
           <IconButton><MenuIcon /></IconButton>
-          <div></div>
+          <div className={`${styles.youtubeLogoIcon} ${styles.allContainers}`} ><YouTubeIcon fontSize='large' sx={{ color:"red" }}/><div>YouTube-Clone</div></div>
         </div>
-        <div className={styles.middleContainer}>
+        <div className={`${styles.middleContainer} ${styles.allContainers}`}>
           
         </div>
-        <div className={styles.rightContainer}>
+        <div className={`${styles.rightContainer} ${styles.allContainers}`}>
           
         </div>
     </div>
