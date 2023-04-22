ddd17c6e50dc905f3c74754c86bb0dd8b1e0c773
Shaikh Yakub Hossain <shaikhyakubhossain@gmail.com>
2023-04-22 13:35:31 +0530

    Improve Home Button

--- src/components/LeftMenu/left-menu.component.jsx
+++ src/components/LeftMenu/left-menu.component.jsx
@@ -1,27 +1,44 @@
-import React from 'react';
-import styles from '../LeftMenu/left-menu.module.css';
-import { Button } from '@mui/material';
-import HomeIcon from '@mui/icons-material/Home';
+import React, { useState } from "react";
+import styles from "../LeftMenu/left-menu.module.css";
+import { Button } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import {leftMenuMainSectionRow1} from '../../constants/btn-list'
 
 function LeftMenu() {
-
-
-  const toggleLeftMenu = () => {
-    
-  }
+  const toggleLeftMenu = () => {};
+  const [btnStyle, setBtnStyle] = useState({background: "rgb(39,39,39)",
+  width: "100%",
+  height: "40px",
+  justifyContent: "left",
+  borderRadius: "10px",});
 
   return (
     <>
-        <div className={styles.mainSectionExpand}>
-          <div className={styles.row1}>
-           <div className={styles.btnContainer}><Button sx={{ width:"100%", justifyContent:"left" }}><div><div><HomeIcon /></div><div className={styles.btnFontContainer}><div className={styles.btnFont}>Home</div></div></div></Button></div>
+      <div className={styles.mainSectionExpand}>
+        <div className={styles.row1}>
+          <div className={styles.btnContainer}>
+            <Button variant="text"
+              sx={
+                btnStyle
+              }
+            >
+              <div>
+                <div className={styles.btnIconContainer}>
+                  <HomeIcon />
+                </div>
+                <div className={styles.btnFontContainer}>
+                  <div className={styles.btnFont}>Home</div>
+                </div>
+              </div>
+            </Button>
           </div>
-          <div className={styles.row2}></div>
         </div>
-        <div className={styles.exploreSection}></div>
-        <div className={styles.moreFromYoutubeSection}></div>
+        <div className={styles.row2}></div>
+      </div>
+      <div className={styles.exploreSection}></div>
+      <div className={styles.moreFromYoutubeSection}></div>
     </>
-  )
+  );
 }
 
-export default LeftMenu
+export default LeftMenu;
---
+++ src/constants/btn-list.js
@@ -0,0 +1,2 @@
+export const leftMenuMainSectionRow1 = ["Home", "Shorts", "Subscriptions"];
+export const leftMenuMainSectionRow2 = ["Library", "History", "Watch later", "Liked videos"];
