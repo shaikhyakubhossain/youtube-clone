3ec8efcf59d36bf06f6b07fc7e4b5a6ce12d335e
Shaikh Yakub Hossain <shaikhyakubhossain@gmail.com>
2023-06-21 08:54:58 +0530

    Deploy Webpage

--- src/components/LeftMenu/left-menu.component.jsx
+++ src/components/LeftMenu/left-menu.component.jsx
@@ -1,25 +1,27 @@
 import React, { useState } from "react";
 import styles from "../LeftMenu/left-menu.module.css";
 import { Button } from "@mui/material";
-import {leftMenuMainSectionRow1} from '../../constants/btn-list'
+import { leftMenuMainSectionRow1 } from "../../constants/btn-list";
 
 function LeftMenu() {
   const toggleLeftMenu = () => {};
-  const [btnStyle, setBtnStyle] = useState({background: "rgb(0,0,0)",
-  width: "100%",
-  height: "40px",
-  justifyContent: "left",
-  borderRadius: "10px",});
+  const [btnStyle, setBtnStyle] = useState({
+    background: "rgb(0,0,0)",
+    width: "100%",
+    height: "40px",
+    justifyContent: "left",
+    borderRadius: "10px",
+  });
   const [previousElemInBtnList, setPreviousElemInBtnList] = useState(null);
 
   const toggleActiveLeftMenuBtn = (e) => {
-    if(previousElemInBtnList !== null){
-      previousElemInBtnList.style.background = "rgb(15  ,15 ,15  )"
+    if (previousElemInBtnList !== null) {
+      previousElemInBtnList.style.background = "rgb(15, 15, 15)";
     }
     e.currentTarget.style.background = "rgb(39,39,39)";
-    console.log("this", e.currentTarget, "style", e.currentTarget.style);  
+    console.log("this", e.currentTarget, "style", e.currentTarget.style);
     setPreviousElemInBtnList(e.currentTarget);
-  }
+  };
 
   return (
     <>
@@ -28,24 +30,22 @@ function LeftMenu() {
           <div className={styles.btnContainer}>
             {leftMenuMainSectionRow1.map((item) => {
               return (
-                <Button variant="text"
-              sx={
-                btnStyle
-              }
-              onClick={(e) => toggleActiveLeftMenuBtn(e)}
-            >
-              <div>
-                <div className={styles.btnIconContainer}>
-                  {item.logo}
-                </div>
-                <div className={styles.btnFontContainer}>
-                  <div className={styles.btnFont}>{item.name}</div>
-                </div>
-              </div>
-            </Button>
-              )
+                <Button
+                  variant="text"
+                  sx={btnStyle}
+                  onClick={(e) => toggleActiveLeftMenuBtn(e)}
+                >
+                  <div className={styles.btnContainerMainMin}>
+                    <div className={styles.btnIconContainer}>
+                      {item.logo}
+                    </div>
+                    <div className={styles.btnFontContainer}>
+                      <div className={styles.btnFont}>{item.name}</div>
+                    </div>
+                  </div>
+                </Button>
+              );
             })}
-            
           </div>
         </div>
         <div className={styles.row2}></div>
