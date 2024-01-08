69285b90bf756a28c2bbe2190909fc8fcc268bbb
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-01-08 16:26:29 +0530

    Improve Video Player

--- src/components/VideoPlayer/video-player.component.jsx
+++ src/components/VideoPlayer/video-player.component.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import PropsTypes from 'prop-types'
+import React from "react";
+import PropsTypes from "prop-types";
+import styles from "./video-player..module.css";
 
-const VideoPlayer = ({embedId}) => {
-
+const VideoPlayer = ({ embedId }) => {
   return (
-    <div>
-        <iframe src={"https://www.youtube.com/embed/" + embedId} frameborder="0"></iframe>
+    <div className={styles.videoPlayer}>
+      <iframe
+        src={"https://www.youtube.com/embed/" + embedId}
+        width="853"
+        height="480"
+        frameborder="0"
+      ></iframe>
     </div>
-  )
+  );
+};
 
-}
-
-    VideoPlayer.propsTypes = {
-        embedId: PropsTypes.string.isRequired
-    }
+VideoPlayer.propsTypes = {
+  embedId: PropsTypes.string.isRequired,
+};
 
 export default VideoPlayer;
