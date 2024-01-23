c63184a8efdd73331bb0f6aae800108c8b07fd21
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-01-23 18:56:01 +0530

    Add Like Button Under Video Title In watch-video Page

--- src/pages/WatchVideo/watch-video.component.jsx
+++ src/pages/WatchVideo/watch-video.component.jsx
@@ -6,10 +6,24 @@ import { connect } from "react-redux";
 import { setFalse } from "../../redux/index";
 import axios from "axios";
 import { Button } from "@mui/material";
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import shortNumber from "short-number";
 
 const WatchVideo = (props) => {
   const params = useParams();
   const [apiData, setApiData] = useState(null);
+  const [muiBtnSx, setMuiBtnSx] = useState({
+    fontSize: "14px",
+    fontWeight: "500",
+    color: "black",
+    height: "36px",
+    background: "rgba(255, 255, 255, 1)",
+    borderRadius: "18px",
+    padding: "0 16px",
+    ":hover": {
+      background: "rgba(255, 255, 255, 0.8)",
+    },
+  });
 
   useEffect(() => {
     props.setFalse();
@@ -18,7 +32,7 @@ const WatchVideo = (props) => {
   }, [params]);
 
   const fetchData = () => {
-    //   axios.get("http://localhost:4000/videoDetail" + params.id)
+    // axios.get("http://localhost:4000/videoDetail" + params.id)
       axios.get('https://youtube-clone-backend-five.vercel.app/videoDetail' + params.id)
       .then((response) => {
         const json = response.data;
@@ -42,23 +56,25 @@ const WatchVideo = (props) => {
               {/* <div className={styles.subscribersCount}>{} subscribers</div> */}
             </div>
             <div className={styles.subscribeBtn}>
-              <Button
-                variant="text"
-                sx={{
-                  fontSize: "14px",
-                  fontWeight: "500",
-                  color: "black",
-                  height: "36px",
-                  background: "rgba(255, 255, 255, 1)",
-                  borderRadius: "18px",
-                  padding: "0 16px",
-                  ":hover": {
-                    background: "rgba(255, 255, 255, 0.8)"
-                  }
-                }}
-              >
+              <Button variant="text" sx={muiBtnSx}>
                 Subscribe
               </Button>
+            </div>
+          </div>
+          <div className={styles.subContainer1RightPart}>
+            <div className={styles.rightPartBtn}>
+            <Button startIcon={<ThumbUpOutlinedIcon />} sx={{
+              ...muiBtnSx,
+              background: "rgba(255, 255, 255, 0.1)",
+              color: "white",
+              borderTopRightRadius: "0",
+              borderBottomRightRadius: "0",
+              ":hover": {
+                  background: "rgba(255, 255, 255, 0.2)",
+                }
+            }}>
+              {shortNumber(parseInt(apiData.statistics.likeCount))}
+            </Button>
             </div>
           </div>
         </div>
