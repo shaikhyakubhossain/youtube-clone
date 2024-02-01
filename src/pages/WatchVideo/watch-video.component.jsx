6a8b92463443badc50841fcd8693b828480391fa
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-02-01 20:34:45 +0530

    Add MUI Skeleton Loading For Video Detail

--- src/pages/WatchVideo/watch-video.component.jsx
+++ src/pages/WatchVideo/watch-video.component.jsx
@@ -12,6 +12,7 @@ import shortNumber from "short-number";
 import { subContainer1RightPart } from "../../constants/btn-list";
 import { mostPopularVideos, videoDetail } from "../../constants/url-list";
 import { fetchMaxAvailableThumbnailResolution } from "../../constants/utils";
+import Skeleton from '@mui/material/Skeleton';
 
 
 const WatchVideo = (props) => {
@@ -192,7 +193,40 @@ const WatchVideo = (props) => {
   };
 
   const whenVideoDetailIsLoading = () => {
-    return <></>;
+    const marginLR = {
+      margin: "0 4px 0 4px"
+    } 
+
+    const marginTB = {
+      margin: "4px 0 4px 0"
+    }
+    return (
+      <>
+      <Skeleton variant="rounded" width={410} height={20} sx={marginTB}/>
+      <div className={styles.videoDetailFlex1} >
+      <Skeleton variant="rounded" width={210} height={20} sx={marginTB}/>
+      <div>
+      <Skeleton variant="circular" width={20} height={20} sx={marginLR}/>
+      <Skeleton variant="circular" width={20} height={20} sx={marginLR}/>
+      <Skeleton variant="circular" width={20} height={20} sx={marginLR}/>
+      <Skeleton variant="circular" width={20} height={20} sx={marginLR}/>
+      <Skeleton variant="circular" width={20} height={20} sx={marginLR}/>
+      </div>
+      </div>
+      <div className={styles.videoDetailLoadingFlex}>
+        <div>
+        <Skeleton variant="circular" width={60} height={60} sx={marginLR}/>
+        <div >
+      <Skeleton variant="rounded" width={210} height={20} sx={marginTB}/>
+      <Skeleton variant="rounded" width={210} height={20} sx={marginTB}/>
+        </div>
+        </div>
+        <div className={styles.subscribeBtnSkeleton}>
+          <Skeleton variant="rounded" width={150} height={40} sx={marginTB}/>
+        </div>
+      </div>
+      </>
+    );
   };
 
   const whenVideoSuggestionIsLoaded = () => {
@@ -201,7 +235,7 @@ const WatchVideo = (props) => {
         <div className={styles.videoSuggestionItemContainer}>
           <div className={styles.videoSuggestionThumbnail}><img src={fetchMaxAvailableThumbnailResolution(item).url} /></div>
           <div>
-          {item.snippet.title.length >= 59 ? <div className={styles.videoSuggestionTitle}>{item.snippet.title.slice(0, 58)}...</div> : <div className={styles.videoSuggestionTitle}>{item.snippet.title}</div>}
+          {item.snippet.title.length >= 58 ? <div className={styles.videoSuggestionTitle}>{item.snippet.title.slice(0, 57)}...</div> : <div className={styles.videoSuggestionTitle}>{item.snippet.title}</div>}
           <div className={styles.videoSuggestionChannelName}>{item.snippet.channelTitle}</div>
           <div className={styles.videoSuggestionViewAndTimeContainer} >
             <div>{shortNumber(parseInt(item.statistics.viewCount))} views</div>
@@ -220,7 +254,6 @@ const WatchVideo = (props) => {
       </>
     )
   }
-
 
   return (
     <div className={styles.mainContainer}>
