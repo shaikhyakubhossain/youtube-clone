596716782b0a2af59bba0a561571589f93cb70be
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-03-08 20:30:37 +0530

    Correct URL

--- src/constants/url-list.js
+++ src/constants/url-list.js
@@ -1,4 +1,4 @@
-export const mostPopularVideos = 'http://localhost:4000/mostPopularVideos';
-export const videoDetail = 'http://localhost:4000/videoDetail';
-// export const mostPopularVideos = 'https://youtube-clone-backend-five.vercel.app/mostPopularVideos';
-// export const videoDetail = 'https://youtube-clone-backend-five.vercel.app/videoDetail';
+// export const mostPopularVideos = 'http://localhost:4000/mostPopularVideos';
+// export const videoDetail = 'http://localhost:4000/videoDetail';
+export const mostPopularVideos = 'https://youtube-clone-backend-five.vercel.app/mostPopularVideos';
+export const videoDetail = 'https://youtube-clone-backend-five.vercel.app/videoDetail';
