61509a565630460c19870c51acf8b02824984e25
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-07-05 15:54:58 +0530

    Improve responsiveness of all leftMenu components

--- src/components/RouterWrapper/router-wrapper.component.jsx
+++ src/components/RouterWrapper/router-wrapper.component.jsx
@@ -14,62 +14,108 @@ import { toggleMinMax } from "../../redux/index";
 
 const RouterWrapper = (props) => {
 
-  const LeftMenuForDesktopRef = useRef(null);
-  const LeftMenuForMobileRef = useRef(null);
+  const leftMenuForDesktopRef = useRef(null);
+  const leftMenuForMobileRef = useRef(null);
+  const homeComponent = useRef(null);
+
   const location = useLocation();
   // const [leftMenuStyle, setLeftMenuStyle] = useState("block")
+  const [screenSize, setscreenSize] = useState({
+    width: window.innerWidth
+  });
 
 
   const toggleMaxMinOfLeftMenu = (props) => {
 
+
+    if(screenSize.width < 1312 && location.pathname.slice(1, 12) !== "watch-video"){
+      homeComponent.current.children[0].style.left = "64px";
+      // console.log("home: ", homeComponent.current.style);
+      // leftMenuForDesktopRef.current.style.zIndex = 2;
+      leftMenuForMobileRef.current.style.display = "block";
+    }
+
+    if(location.pathname.slice(1, 12) !== "watch-video" || screenSize.width < 1312){
+
+      if(!props.isMaximized){
+      toggleMinMax();
+
+        leftMenuForDesktopRef.current.className = styles.LeftMenuForDesktopActive;
+        //  leftMenuForDesktopRef.current.style.display = "block";
+        // setLeftMenuStyle("block")
+        console.log("blocked");
+      }
+      if(props.isMaximized){
+        leftMenuForDesktopRef.current.className = styles.LeftMenuForDesktopInactive;
+        //  leftMenuForDesktopRef.current.style.display = "none";
+        // setLeftMenuStyle("none")
+        console.log("noned");
+      }
+
+    }
     if(location.pathname.slice(1, 12) === "watch-video"){
       toggleMinMax();
       console.log("hello");
       if(!props.isMaximized){
-        LeftMenuForDesktopRef.current.className = styles.LeftMenuForDesktopActive;
-        //  LeftMenuForDesktopRef.current.style.display = "block";
+        leftMenuForDesktopRef.current.className = styles.LeftMenuForDesktopActive;
+        //  leftMenuForDesktopRef.current.style.display = "block";
         // setLeftMenuStyle("block")
         console.log("blocked");
       }
       if(props.isMaximized){
-        LeftMenuForDesktopRef.current.className = styles.LeftMenuForDesktopInactive;
-        //  LeftMenuForDesktopRef.current.style.display = "none";
+        leftMenuForDesktopRef.current.className = styles.LeftMenuForDesktopInactive;
+        //  leftMenuForDesktopRef.current.style.display = "none";
         // setLeftMenuStyle("none")
         console.log("noned");
       }
     }
-    else{
-      LeftMenuForDesktopRef.current.className = styles.LeftMenuForDesktopInactive;
+    if(location.pathname.slice(1, 12) !== "watch-video" && screenSize.width >= 1312){
+      leftMenuForDesktopRef.current.className = styles.LeftMenuForDesktopInactive;
       console.log("hiiiiiii")
       if(!props.isMaximized){
-         LeftMenuForDesktopRef.current.style.display = "block";
-         LeftMenuForMobileRef.current.style.display = "none";
+        homeComponent.current.children[0].style.left = "244px";
+         leftMenuForDesktopRef.current.style.display = "block";
+         leftMenuForMobileRef.current.style.display = "none";
         // setLeftMenuStyle("block")
         console.log("blocked");
       }
       if(props.isMaximized){
-         LeftMenuForDesktopRef.current.style.display = "none";
-         LeftMenuForMobileRef.current.style.display = "block";
+         toggleMinMax();
+        homeComponent.current.children[0].style.left = "64px";
+         leftMenuForDesktopRef.current.style.display = "none";
+         leftMenuForMobileRef.current.style.display = "block";
         // setLeftMenuStyle("none")
         console.log("noned");
       }
     }
   }
 
+  const handleResize = () => {
+    setscreenSize({
+      width: window.innerWidth
+    })
+  }
+
   useEffect(() => {
-    // LeftMenuForDesktopRef.current.style.display = leftMenuStyle;
+    // leftMenuForDesktopRef.current.style.display = leftMenuStyle;
     // console.log("location", location.pathname.slice(1, 12));
+    
+    window.addEventListener('resize', handleResize);
+    console.log("width: ", screenSize.width)
     toggleMaxMinOfLeftMenu(props);
-  }, [props.isMaximized, location])
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };  
+  }, [props.isMaximized, location, screenSize])
 
 
   return (
     <>
     <Nav />
-    <div className={styles.LeftMenuForDesktopInactive} ref={LeftMenuForDesktopRef}><LeftMenuForDesktop /></div>
-    <div ref={LeftMenuForMobileRef}><LeftMenuForMobile /></div>
+    <div className={styles.LeftMenuForDesktopInactive} ref={leftMenuForDesktopRef}><LeftMenuForDesktop /></div>
+    <div ref={leftMenuForMobileRef}><LeftMenuForMobile /></div>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<div ref={homeComponent}><Home /></div>} />
       <Route path="watch-video/:id" element={<WatchVideo />} />
     </Routes>
     </>
