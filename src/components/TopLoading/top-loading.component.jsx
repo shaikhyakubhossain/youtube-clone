a6eaa3d10aad4b80e2142eb01c0dfaaab91e796b
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-10-05 21:45:04 +0530

    Update TopLoading component, update redux store.js

--- src/components/TopLoading/top-loading.component.jsx
+++ src/components/TopLoading/top-loading.component.jsx
@@ -2,18 +2,15 @@ import styles from './top-loading.module.css';
 import { connect } from 'react-redux';
 
 const TopLoading = (props) => {
+    console.log("props", props);
     return (
-        <div className={styles.mainContainer} style={{width: props.showLoading ? "80%" : "0%", transition: "width 2s"}}>
-            <div className={styles.loadingContainer}>
-                <div className={styles.loadingBar}></div>
-            </div>
-        </div>
+        <div className={styles.mainContainer} style={{width: props.showLoading ? "80%" : "0%", transition: props.showLoading ? "width 2s" : "none"}}></div>
     );
 }
 
 const mapStateToProps = (state) => {
     return {
-      showLoading: state.showLoading
+      showLoading: state.topLoading.showLoading
     }
   }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers } from "redux";
 import leftMenuReducer from "./leftMenu/left-menu-reducer";
 import topLoadingReducer from "./TopLoading/top-loading-reducer";
 
-const store = createStore(combineReducers({
+const rootReducer = combineReducers({
     leftMenu: leftMenuReducer,
     topLoading: topLoadingReducer
-}));
+});
+
+const store = createStore(rootReducer);
 
 export default store;
